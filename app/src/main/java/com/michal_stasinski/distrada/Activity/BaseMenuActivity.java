c3281905fcefdc59a45e1ccb7518126829f9d9a2
Miodkowy <michal.stasinski@poczta.fm>
2017-03-25 10:42:46 +0100

    nowa arch

---
+++ app/src/main/java/com/michal_stasinski/distrada/Activity/BaseMenuActivity.java
@@ -0,0 +1,15 @@
+package com.michal_stasinski.distrada.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.michal_stasinski.distrada.R;
+
+public class BaseMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_menu);
+    }
+}
